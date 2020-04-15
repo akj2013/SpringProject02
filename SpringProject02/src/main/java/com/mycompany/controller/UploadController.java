4553520d0cecb723de7962342427472aa66ff2c2
akj2013 <48812384+akj2013@users.noreply.github.com>
2020-04-15 19:09:23 +0900

    0415 SLU 첨부파일 상세처리
    1. 파일 확장자와 크기 사전 처리
    2. 중복방지를 위한 날짜형식의 폴더 생성 처리

--- SpringProject02/src/main/java/com/mycompany/controller/UploadController.java
+++ SpringProject02/src/main/java/com/mycompany/controller/UploadController.java
@@ -1,6 +1,8 @@
 package com.mycompany.controller;
 
 import java.io.File;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -45,24 +47,59 @@ public class UploadController {
 	}
 	
 	@PostMapping("/uploadAjaxAction")
-	public void uploadAjaxAction(MultipartFile[] uploadFile, Model model) {
+	public void uploadAjaxAction(MultipartFile[] uploadFile) {
 		String uploadFolder = "C:\\upload";
+		// 결과 로그 출력을 위한 변수 생성
+		int successedFileCount = uploadFile.length;
+		int failedFileCount = 0;
+		
+		// 업로드 폴더 생성
+		File uploadPath = new File(uploadFolder, getFolder());
+		log.info("upload path: " + uploadPath);
+		// 폴더 이름이 존재하지 않는다면, 생성한다.
+		if (uploadPath.exists() == false) {
+			boolean makeFolder = uploadPath.mkdirs();
+			if (makeFolder) {
+				log.info("오늘 날짜의 새로운 폴더를 생성하였습니다." + uploadPath.getName()); 
+			}
+		}
+		
 		for (MultipartFile multipartFile : uploadFile) {
 			log.info("----------------------------------");
 			log.info("Upload File Name : " + multipartFile.getOriginalFilename());
-			log.info("Upload File Size : " + multipartFile.getSize());
+			log.info("Upload File Size : " + multipartFile.getSize() + "bytes");
 			
 			String uploadFileName = multipartFile.getOriginalFilename();
 			uploadFileName = uploadFileName.substring(uploadFileName.lastIndexOf("\\") + 1);
 			log.info("only file name : " + uploadFileName);
 			
-			File saveFile = new File(uploadFolder, multipartFile.getOriginalFilename());
+			// File saveFile = new File(uploadFolder, multipartFile.getOriginalFilename());
+			// 새로 생성된 폴더에 파일을 저장한다.
+			File saveFile = new File(uploadPath, uploadFileName);
 			
 			try {
 				multipartFile.transferTo(saveFile);
 			} catch (Exception e) {
 				log.error(e.getMessage());
+				failedFileCount += 1; // 실패한 경우, 하나씩 더해준다.
+				successedFileCount -= 1; // 성공한 경우, 하나씩 빼준다.
+			} finally {
 			}
+			// 결과 로그 출력
+			log.info("TOTAL : " + uploadFile.length + "건, " + "SUCCESS : " + successedFileCount + "건, " + "FAIL : " + failedFileCount + "건");
 		}
+	}
+	
+	/**
+	 * 중복된 이름의 첨부파일 처리 </br>
+	 * 파일 이름이 중복되지 않도록 처리하는 방법으로, 현재 시간을 폴더 이름으로 사용하기 위해 </br>
+	 * 폴더 이름을 생성한다.
+	 * @return FolderName
+	 */
+	private String getFolder() {
+		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+		Date date = new Date();
+		String str = sdf.format(date);
+		return str.replace("-", File.separator); // separator = /
 	}
 }
--- SpringProject02/src/main/webapp/WEB-INF/views/upload/uploadAjax.jsp
+++ SpringProject02/src/main/webapp/WEB-INF/views/upload/uploadAjax.jsp
@@ -17,6 +17,7 @@
 		crossorigin="anonymous"></script>
 	
 	<script>
+	/* 아래 새로운 처리를 위해 코멘트 아웃
 		$(document).ready(function(){
 			$("#uploadBtn").on("click",function(e) {
 				var formData = new FormData();
@@ -36,9 +37,63 @@
 						data: formData,
 						type: 'POST',
 						success: function(result) {
-							alert("Uploaded");
+							alert("Uploaded 22");
 						}
 				});
+			});
+		});
+		*/
+	
+	
+		/*
+		파일 확장자나 크기의 사전 처리 p.506
+		첨부파일을 이용한 웹 공격을 막기 위해 확장자를 제한한다.
+		*/
+		$(document).ready(function(){
+			var regex = new RegExp("(.*?)\.(exe|sh|zip|alz)$"); // 여기에 해당하는 파일은 첨부 불가
+			var maxSize = 5242880; // 5MB
+			
+			function checkExtension(fileName, fileSize) {
+				// 파일 사이즈 체크
+				if(fileSize >= maxSize) {
+					alert("파일 사이즈 초과");
+					return false;
+				}
+				// 파일 확장자 체크
+				if(regex.test(fileName)) {
+					alert("해당 종류의 파일은 업로드할 수 없습니다.");
+					return false;
+				}
+				// 위에서부터 모든 체크를 통과하고 여기까지 내려오는 경우에만 true를 리턴한다.
+				return true;
+			}
+			
+			$("#uploadBtn").on("click", function(e) {
+				var formData = new FormData();
+				var inputFile = $("input[name='uploadFile']");
+				var files = inputFile[0].files;
+				
+				console.log(files);
+				
+				for(var i=0; i<files.length; i++) {
+					// 체크 처리를 통과하지 못하면 업로드를 종료한다.
+					if(!checkExtension(files[i].name, files[i].size)) {
+						return false;
+					}
+					// 체크 처리를 통과한 파일은 업로드 대상으로 지정한다.
+					formData.append("uploadFile", files[i]);
+				}
+				
+				$.ajax({
+					url: 'uploadAjaxAction',
+					processData: false,
+					contentType: false,
+					data: formData,
+					type: 'POST',
+					success: function(result) {
+						alert("Uploaded 11");
+					}
+				}); //$.ajax
 			});
 		});
 	</script>
